cb772e161a9c68450fe2f02d32f58916e066549d
Nathália Beatriz <94134475+nathaliabeatriz@users.noreply.github.com>
2023-03-24 16:20:51 -0300

    Create mdc e mmc.cpp

---
+++ matemática/divisibilidade/mdc e mmc.cpp
@@ -0,0 +1,21 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+//faz o mdc de dois números
+int mdc(int a, int b){
+	if(b==0) return a;
+	mdc(b, a%b);
+}
+
+//faz o mmc de dois números
+long long mmc(int a, int b){
+	return (a/mdc(a,b)*1LL*b);
+}
+
+int main(){
+	int a, b;
+	cin >> a >> b;
+	cout << "MDC: " << mdc(a, b) << endl;
+	cout << "MMC: " << mmc(a, b) << endl;
+	return 0;
+}
