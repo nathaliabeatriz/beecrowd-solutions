67956c572c5b627d2485bf6e71a3d6d3ef0e4aa5
Nathália Beatriz <94134475+nathaliabeatriz@users.noreply.github.com>
2023-03-31 16:42:37 -0300

    Create mdc_sequenciado.cpp

---
+++ matemática/divisibilidade/mdc_sequenciado.cpp
@@ -0,0 +1,30 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int mdc(int a, int b){
+	if(b==0){
+		return a;
+	}
+	return mdc(b, a%b);
+}
+
+int main(){
+	int n, aux, mdc_var;
+	cin >> n;
+	vector<int>nums;
+	
+	for(int i=0; i<n; i++){
+		cin >> aux;
+		nums.push_back(aux);
+	}
+	
+	mdc_var = nums[0];
+	
+	for(int i=1; i<nums.size(); i++){
+		mdc_var = mdc(mdc_var, nums[i]);
+	}
+	
+	cout << mdc_var << endl;
+	
+	return 0;
+}
